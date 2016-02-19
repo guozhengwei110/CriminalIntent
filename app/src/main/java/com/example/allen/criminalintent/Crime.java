d23ec60142e83a15da86d421585ba40f9952b6b8
guozhengwei110 <18434367921@139.com>
2016-02-20 00:17:10 +0800

    Initial commit

---
+++ app/src/main/java/com/example/allen/criminalintent/Crime.java
@@ -0,0 +1,55 @@
+package com.example.allen.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * Crime
+ *
+ *
+ * @author: Allen
+
+ * @time: 2016/1/26 7:40
+ *
+ * CriminalIntent的模型层
+ */
+public class Crime {
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public Crime(){
+        mId = UUID.randomUUID();    //生成唯一的标识
+        mDate = new Date();
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+
+    public Date getDate() {
+
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+}
---
+++ app/src/main/java/com/example/allen/criminalintent/CrimeActivity.java
@@ -0,0 +1,24 @@
+package com.example.allen.criminalintent;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CrimeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.crime_main);
+
+        FragmentManager fm = getFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+        if (fragment == null) {
+            fragment = new CrimeFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragmentContainer, fragment)
+                    .commit();
+        }
+    }
+}
