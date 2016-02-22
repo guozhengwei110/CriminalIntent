a93cd309ef9e4ad159353032f5bc04e4cbdf429f
guozhengwei110 <18434367921@139.com>
2016-02-23 00:07:39 +0800

    创建日期对话框

--- app/src/main/java/com/example/allen/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/example/allen/criminalintent/CrimeListFragment.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -29,6 +31,7 @@ public class CrimeListFragment extends ListFragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);        //设置fragment接受onCreateOptionMenu()方法
         getActivity().setTitle(R.string.crimes_title);
         mCrimes = CrimeLab.get(getActivity()).getCrimes();
         CrimeAdapter adapter = new CrimeAdapter(mCrimes);
@@ -80,4 +83,9 @@ public class CrimeListFragment extends ListFragment {
         }
     }
 
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.fragment_crime_list, menu);     //将定义的菜单项目填充到Menu实例
+    }
 }
