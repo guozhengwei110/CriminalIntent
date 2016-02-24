709d0aa0c7399b00285493e26f68278d97f0b6d8
guozhengwei110 <18434367921@139.com>
2016-02-25 01:13:51 +0800

    照相机照相，保存照片

--- app/src/main/java/com/example/allen/criminalintent/CrimeCameraFragment.java
+++ app/src/main/java/com/example/allen/criminalintent/CrimeCameraFragment.java
@@ -1,6 +1,7 @@
 package com.example.allen.criminalintent;
 
 import android.app.Fragment;
+import android.content.Context;
 import android.hardware.Camera;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -12,8 +13,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
 
 /**
  * CrimeCameraFragment
@@ -25,18 +29,62 @@ public class CrimeCameraFragment extends Fragment {
     private static final String TAG = "CrimeCameraFragment";
     private Camera mCamera;
     private SurfaceView mSurfaceView;
+    private View mProgressContainer;
+
+    private Camera.ShutterCallback mShutterCallback = new Camera.ShutterCallback() {
+        public void onShutter() {
+            mProgressContainer.setVisibility(View.VISIBLE);
+        }
+    };
+
+    private Camera.PictureCallback mJepgCallback = new Camera.PictureCallback() {
+
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+            String filename = UUID.randomUUID().toString() + ".jpg";
+            FileOutputStream os = null;
+            boolean success = true;
+
+            try {
+                os = getActivity().openFileOutput(filename, Context.MODE_PRIVATE);
+                os.write(data);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                Log.e(TAG, "---------Error writing to file-----------" + filename, e);
+                success = false;
+            } finally {
+                try {
+                    if (os != null)
+                        os.close();
+                } catch (Exception e) {
+                    Log.e(TAG, "---------Error closing file---------" + filename, e);
+                }
+            }
+
+            if (success)
+                Log.i(TAG, "-----------JEPG saved at--------------- " + filename);
+            getActivity().finish();
+        }
+    };
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_crime_camera, container, false);
+
+        mProgressContainer = v.findViewById(R.id.crime_camera_progressContainer);
+        mProgressContainer.setVisibility(View.INVISIBLE);
+
         Button takePictrueButton = (Button) v.findViewById(R.id.crime_camera_takePictrueButton);
         takePictrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getActivity().finish();
+                if (mCamera != null)
+                    mCamera.takePicture(mShutterCallback, null, mJepgCallback);
             }
         });
+
         mSurfaceView = (SurfaceView) v.findViewById(R.id.crime_camera_surface);
         SurfaceHolder holder = mSurfaceView.getHolder();
 //        holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
